471f8506a62a1b0e37ced67e77b7a23aeac92641
Felipe <antoniofelipesilva@live.com>
2024-03-29 18:23:47 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Movie myMovie = new Movie();
+
+        myMovie.nameMovie = "Cars";
+        myMovie.ageMovie = 2010;
+        myMovie.movieDuration = 180;
+
+        System.out.println(myMovie);
+        System.out.println(myMovie.nameMovie);
+
+    }
+}
---
+++ src/Movie.java
@@ -0,0 +1,7 @@
+public class Movie {
+    String nameMovie;
+    int ageMovie;
+    boolean packageIncluded;
+    int assessment;
+    int movieDuration;
+}
