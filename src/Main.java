182cf4e26343962d83c534ab40ec4bf078278531
Felipe <antoniofelipesilva@live.com>
2024-03-31 20:32:53 -0300

    new serie page

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.model.Title;
 
 public class Main {
@@ -10,15 +11,27 @@ public class Main {
         myMovie.setTitleDuration(180);
         myMovie.setPackageIncluded(true);
 
-        myMovie.showTechnicalSheet();
         myMovie.toAssess(8);
         myMovie.toAssess(5);
         myMovie.toAssess(3);
 
-        System.out.println(myMovie.getAgeTitle());
         System.out.println(myMovie.getNameTitle());
+        System.out.println(myMovie.getAgeTitle());
         System.out.println(myMovie.getTitleDuration() + "\n");
         System.out.println(myMovie.getTotalAssessment());
         System.out.println(myMovie.getMedia());
+
+        Serie supernatural  = new Serie();
+
+        supernatural.setNameTitle("Supernatural");
+        supernatural.setAgeTitle(2003);
+        supernatural.setTemples(15);
+        supernatural.setEpisodes(23);
+        supernatural.setMinutesEpisode(50);
+
+
+        System.out.println(supernatural.getNameTitle());
+        System.out.println(supernatural.getAgeTitle());
+        System.out.println(supernatural.getTitleDuration());
     }
 }
--- src/br/com/alura/screenmatch/model/Serie.java
+++ src/br/com/alura/screenmatch/model/Serie.java
@@ -2,4 +2,44 @@ package br.com.alura.screenmatch.model;
 
 public class Serie extends Title {
     private int episodes;
+    private int temples;
+    private boolean active;
+    private int minutesEpisode;
+
+    public int getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(int episodes) {
+        this.episodes = episodes;
+    }
+
+    public int getTemples() {
+        return temples;
+    }
+
+    public void setTemples(int temples) {
+        this.temples = temples;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getMinutesEpisode() {
+        return minutesEpisode;
+    }
+
+    public void setMinutesEpisode(int minutesEpisode) {
+        this.minutesEpisode = minutesEpisode;
+    }
+
+    @Override
+    public int getTitleDuration() {
+        return temples * episodes * minutesEpisode;
+    }
 }
